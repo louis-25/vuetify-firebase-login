96d93a4addca3805b27e6820e4e319128a0adc45
DongHyeon <20151577@vision.hoseo.edu>
2020-09-03 00:37:02 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vuetify-firebase-login
-test version
+# vue-test
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyBtDxUswKTPwLt984lRRnR8I2KOVCeoSlc',
+  authDomain: 'vue-login-e17f8.firebaseapp.com',
+  databaseURL: 'https://vue-login-e17f8.firebaseio.com',
+  projectId: 'vue-login-e17f8',
+  storageBucket: 'vue-login-e17f8.appspot.com',
+  messagingSenderId: '945818526539',
+  appId: '1:945818526539:web:36d68a97ff943a6726bece',
+  measurementId: 'G-4YMTSYECTY'
+}
